df458217872a71f2bd9fdc4d6dc96f7969781355
mmelgies <michalmelgies95@gmail.com>
2023-05-12 10:19:06 +0200

    FirstTryJS

---
+++ firstJS/firstJS.js
@@ -0,0 +1,16 @@
+function firstbutton() {
+    document.getElementById('data').innerHTML = Date();
+  }
+
+function changehtmlcontent() {
+    document.getElementById("demo").innerHTML = "Hello JavaScript!";
+}
+
+function Turnonthebulb(){
+    document.getElementById('myImage').src='https://www.w3schools.com/js/pic_bulbon.gif';
+}
+
+function Turnoffthebulb() {
+    document.getElementById('myImage').src='https://www.w3schools.com/js/pic_bulboff.gif';
+}
+
