b190f4627b2c4bdc33654598ba9e47322c31c241
mmelgies <michalmelgies95@gmail.com>
2023-05-15 01:20:50 +0200

    exploring the functions

--- firstJS/firstJS.js
+++ firstJS/firstJS.js
@@ -14,3 +14,25 @@ function Turnoffthebulb() {
     document.getElementById('myImage').src='https://www.w3schools.com/js/pic_bulboff.gif';
 }
 
+function adding(a, b){
+    //myDiv.innerHtml= add()
+   // let text = "HELLO";
+    //document.body.innerHTML = 5+6;
+    return a + b;
+}
+
+//myDiv.innerHtml= add();
+//document.body.innerHTML = 5+6;
+function MyMessage(n)
+{
+    return "co";
+}
+
+
+function myFunction(a, b) {
+    return a * b;
+  }
+
+function toCelsius(f) {
+    return (5/9) * (f-32);
+}
