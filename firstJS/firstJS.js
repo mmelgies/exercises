86aad3b359769335727f198c9331d67000e93d82
mmelgies <michalmelgies95@gmail.com>
2023-05-18 10:02:27 +0200

    first functions,objects,events

--- firstJS/firstJS.js
+++ firstJS/firstJS.js
@@ -25,14 +25,50 @@ function adding(a, b){
 //document.body.innerHTML = 5+6;
 function MyMessage(n)
 {
-    return "co";
+    //return "co";
+    document.getElementById("forMessage").innerHTML = "whaaat";
 }
 
 
-function myFunction(a, b) {
-    return a * b;
+function multiplyFunction(a, b) {
+    //return a * b;
+    document.getElementById("multiply").innerHTML = a * b;
   }
 
 function toCelsius(f) {
-    return (5/9) * (f-32);
+    //return (5/9) * (f-32);
+    document.getElementById("tocelsius").innerHTML = (5/9) * (f-32);
 }
+
+function toCelsius2(f) {
+    return (5/9) * (fahrenheit-32);
+}
+
+function toCelciusMix(y) {
+    let x = toCelsius2(y);
+    document.getElementById("celsius2").innerHTML = x;
+    //var text = "The temperature is " + toCelsius2(77) + " Celsius";
+    //return text;
+}
+
+
+function pomnoz(a,b){
+        return a*b;
+    }
+
+function podziel(x){
+    return x/2;
+}
+
+let car = "FIAT";
+const carType = {type:"Fiat", model:"500", color:"white"};
+
+const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+
