af6da8a58edd48fe85bf56e7e503f6ef1ef587f6
mmelgies <michalmelgies95@gmail.com>
2023-06-13 10:59:33 +0200

    switch

--- secondJS/secondJS.js
+++ secondJS/secondJS.js
@@ -1,33 +1,52 @@
-let text;
-const today = new Date();
-const someday = new Date();
-someday.setFullYear(2001, 0, 14);
 
-/*if (someday > today) {
-  text = "Today is before January 14, 2100.";
-} else {
-  text = "Today is after January 14, 2100.";
+/*let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday";
 }
+document.getElementById("demo").innerHTML = "Today is " + day;*/
 
-function check(x){
- 
-  if (x < 18) text = "Too young to buy alcohol"
-  else text = "it's alright, you can buy an alcohol"
-  return text
-}*/
+let day= "Monday"
 
 
-//document.getElementById("demo").innerHTML = check(20)//Boolean(10 > 9);
-
-//document.getElementById("test").innerHTML = 2 > 10;
-
-function checkAge() {
-  let alcoholable;
-  let age = Number(document.getElementById("age").value);
-  if (isNaN(age)) {
-    alcoholable = "Input is not a number";
-  } else {
-    alcoholable = (age < 18) ? "Too young" : "Old enough";
-  }
-  document.getElementById("demo").innerHTML = alcoholable + " to vote";
+switch (day) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday";
 }
+document.getElementById("demo").innerHTML = "Today is " + day;
